0f37360682b5ad6bec0f2fa877d508a5064e47b1
Drinoff <drinoff@gmail.com>
2021-11-06 20:45:45 +0100

    safari landing video solution

--- src/views/homePage.js
+++ src/views/homePage.js
@@ -2,19 +2,33 @@ import { html } from 'https://unpkg.com/lit-html?module'
 import { getInstaVideos } from '../api/data.js'
 
 const homeTemplate = (data, onsubmit) => html`
-
+${
+  /^((?!chrome|android).)*safari/i.test(navigator.userAgent)
+    ? html`
         <video
-        playsinline
           type="video/m4v"
+          controls
+          playsinline
           autoplay
           loop
           muted
-          controls
+          preload="auto"
+        >
+          <source src="../../assets/landingVideo.m4v" />
+        </video>
+      `
+    : html`
+        <video
+          type="video/webm"
+          autoplay
+          loop
+          muted
+          
         >
           <source src="../../assets/landingVideo.webm" />
         </video>
-      
-
+      `
+}
 <div class="description">
     <p>Field Conspiracy is a social experiment & a record label, which nurtures community and creativity. It
         aims to be a playground of creative space for musicians, where the inspiration and courage to be free in
