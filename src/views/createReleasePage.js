2dff4699c1f4ef92846e73fe60c55fe40736b044
Drinoff <drinoff@gmail.com>
2021-11-21 08:55:22 +0100

    added embed options to releases page

--- src/views/createReleasePage.js
+++ src/views/createReleasePage.js
@@ -1,62 +1,76 @@
-import { html } from 'https://unpkg.com/lit-html?module';
-import { createRelease } from '../api/data.js';
+import { html } from "https://unpkg.com/lit-html?module";
+import { createRelease } from "../api/data.js";
 
+const createReleaseTemplate = (onSubmit) => html`
+    <section id="editArtistSection">
+        <form @submit=${onSubmit} id="edit-form">
+            <h1>Create Release</h1>
+            <div class="editContainer">
+                <div>
+                    <label for="name">Artist:</label>
+                    <input id="artist" type="text" value="" name="artist" />
+                </div>
 
-const createReleaseTemplate = (onSubmit) => html `
-<section id="editArtistSection">
-    <form @submit=${onSubmit} id="edit-form">
-        <h1>Create Release</h1>
-        <div class="editContainer">
-            <div>
-                <label for="name">Artist:</label>
-                <input id="artist" type="text" value='' name="artist">
-            </div>
+                <div>
+                    <label for="track">Track:</label>
+                    <input id="track" name="track" value="" name="track" />
+                </div>
 
-            <div>
-                <label for="track">Track:</label>
-                <input id="track" name="track" value = '' name="track" >   
-            </div>
+                <div>
+                    <label for="URL">Image:</label>
+                    <input id="URL" name="URL" value="" name="URL" />
+                </div>
 
-            <div>
-                <label for="URL">Image:</label>
-                <input id="URL" name="URL" value = '' name="URL" >   
+                <div>
+                    <label for="bandCamp">Bandcamp:</label>
+                    <input
+                        type="text"
+                        name="bandCamp"
+                        value=""
+                        id="bandcampEdit"
+                    />
+                </div>
+
+                <div>
+                    <label for="embed">Embed</label>
+                    <input type="text" name="embed" value="" />
+                </div>
+                <input
+                    type="submit"
+                    class="editArtistButton"
+                    value="Create Release"
+                />
             </div>
-            
-            <div>
-                <label for="bandCamp">Bandcamp:</label>
-                <input type="text" name="bandCamp" value='' id="bandcampEdit" />
-            </div>
-            <input type="submit" class="editArtistButton" value="Create Release">
-        </div>
-    </form>
-</section>
+        </form>
+    </section>
 `;
 
 export async function createReleasePage(ctx) {
-    ctx.render(createReleaseTemplate(onSubmit))
+    ctx.render(createReleaseTemplate(onSubmit));
 
     async function onSubmit(e) {
         e.preventDefault();
-        const editForm = document.getElementById('edit-form');
+        const editForm = document.getElementById("edit-form");
         let formData = new FormData(editForm);
 
-        let artist = formData.get('artist');
-        let track = formData.get('track');
-        let URL = formData.get('URL');
-        let bandCamp = formData.get('bandCamp');
-        let id = (Math.random() + 1).toString(36).substring(2);;
-        console.log(id)
+        let artist = formData.get("artist");
+        let track = formData.get("track");
+        let URL = formData.get("URL");
+        let bandCamp = formData.get("bandCamp");
+        let embed = formData.get('embed');
+        let id = (Math.random() + 1).toString(36).substring(2);
+        console.log(id);
 
         const body = {
             artist,
             track,
             URL,
             bandCamp,
-            id
-        }
-        await createRelease(body)
+            id,
+            embed
+        };
+        await createRelease(body);
         ctx.setUserNav();
-        ctx.page.redirect('/releases');
-
+        ctx.page.redirect("/releases");
     }
 }
--- src/views/releasesPage.js
+++ src/views/releasesPage.js
@@ -2,61 +2,78 @@ import { html } from "https://unpkg.com/lit-html?module";
 import { getReleases } from "../api/data.js";
 import { deleteRelease } from "../api/data.js";
 
-const releasesTemplate = (data) => html `
-  <section class="releasesWrapper">${data.length !==0? data.map(releaseCard): 'No Releases Currently'}</section>
+const releasesTemplate = (data) => html`
+    <section class="releasesWrapper">
+        ${data.length !== 0 ? data.map(releaseCard) : "No Releases Currently"}
+    </section>
 `;
 
-const releaseCard = (item) => html `
-  <article class="singleRelease">
-    <a href=${item[1].bandCamp} target="_blank">
-      <div class="imgWrapper">
-        <img class="releaseImg" src=${item[1].URL} alt="releaseImage" />
-      </div>
-      <div class="releaseInfo">
-        <p>${item[1].artist}</p>
-        <p class="beforeElement">${item[1].track}</p>
-      </div>
-      <div class="mobileReleaseInfo">
-        <p class="mobileArtist">${item[1].artist}</p>
-        <p class="mobileTrack">${item[1].track}</p>
-      </div>
-    </a>
-    <div class="creativesAdminButtons">
-            ${(sessionStorage.getItem("email") === 'fieldconspiracy@gmail.com')
-            ?
-            html`<a class="editButton" href="/releases/edit/${item[0]}">Edit</a>
-            <button  id = ${item[0]} href='javascript:void(0)' class="creativesDeleteButton">Delete</button>`
-            : 
-            html``}
+const releaseCard = (item) => html`
+    <article class="singleRelease">
+    <div class="mobileReleaseInfo">
+                <p class="mobileArtist">${item[1].artist}</p>
+                <p class="mobileTrack">${item[1].track}</p>
+            </div>
+        <a href=${item[1].bandCamp} target="_blank">
+            <div class="imgWrapper">
+                <img class="releaseImg" src=${item[1].URL} alt="releaseImage" />
+            </div>
+            <div class="releaseInfo">
+                <p>${item[1].artist}-${item[1].track}</p>
+            </div>
+            
+        </a>
+        <iframe
+            width="100%"
+            height="120px"
+            scrolling="no"
+            frameborder="no"
+            allow="autoplay"
+            src=${item[1].embed}
+        >
+        </iframe>
+        <div class="creativesAdminButtons">
+            ${sessionStorage.getItem("email") === "fieldconspiracy@gmail.com"
+                ? html`<a class="editButton" href="/releases/edit/${item[0]}"
+                          >Edit</a
+                      >
+                      <button
+                          id=${item[0]}
+                          href="javascript:void(0)"
+                          class="creativesDeleteButton"
+                      >
+                          Delete
+                      </button>`
+                : html``}
         </div>
-  </article>
+    </article>
 `;
 
 export async function releasesPage(ctx) {
-  let dataObj = await getReleases();
-  let data;
-  if(dataObj===null){
-    data = [];
-  }else{
-    data = Object.entries(dataObj);
-  }
-  
-  ctx.render(releasesTemplate(data, releaseCard));
-
-  const delButton = document.getElementsByClassName("releasesWrapper")[0];
-  delButton.addEventListener("click", (e) => {
-    if (e.target.textContent === "Delete") {
-      e.preventDefault();
-      const id = e.target.id;
-      onDelete(id);
+    let dataObj = await getReleases();
+    let data;
+    if (dataObj === null) {
+        data = [];
+    } else {
+        data = Object.entries(dataObj);
     }
-  });
 
-  async function onDelete(id) {
-    const confirmed = confirm("Are you sure you want to delete the item?");
-    if (confirmed) {
-      await deleteRelease(id);
-      ctx.page.redirect("/releases");
+    ctx.render(releasesTemplate(data, releaseCard));
+
+    const delButton = document.getElementsByClassName("releasesWrapper")[0];
+    delButton.addEventListener("click", (e) => {
+        if (e.target.textContent === "Delete") {
+            e.preventDefault();
+            const id = e.target.id;
+            onDelete(id);
+        }
+    });
+
+    async function onDelete(id) {
+        const confirmed = confirm("Are you sure you want to delete the item?");
+        if (confirmed) {
+            await deleteRelease(id);
+            ctx.page.redirect("/releases");
+        }
     }
-  }
 }
